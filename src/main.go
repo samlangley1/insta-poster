5d3a2e202fddf8628590c26b38ac45d4ed957be8
samlangley1 <samlangley89@hotmail.com>
2023-12-22 20:51:03 +0000

    add http_proxy and proxying request support

--- src/main.go
+++ src/main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+        "log"
+        "os"
   "go-insta/instagram"
   "go-insta/filesystem"
   "go-insta/config"
@@ -15,33 +16,37 @@ func init() {
   }
 }
 
-func main() { 
+func main() {
   // Generate new config object from config package
   conf := config.New()
 
   // Get random image file from image directory
   image, fileName, err := filesystem.GetRandomContent(conf.Filesystem.ImageDirectory)
   if err != nil {
-    log.Fatal(err)
+          log.Fatalf("failed to fetch image content: %s", err)
   }
 
   // Log into instagram account and create session
   log.Println("logging into: " + conf.Instagram.Username)
   sess, err := instagram.CreateSession(conf.Instagram.Username, conf.Instagram.Password)
   if err != nil {
-    log.Fatal(err)
+          log.Fatalf("failed to log into account: %s", err)
   }
 
+  err = sess.SetProxy(os.Getenv("HTTP_PROXY"), true, true)
+  if err != nil {
+          log.Fatalf("failed to set proxy: %s", err)
+          }
   // Post random image file to Instagram
   err = instagram.PostContent(sess, image)
   if err != nil {
-    log.Fatal(err)
+          log.Fatalf("Failed to post content: %s", err)
   }
   log.Println("uploaded content to instagram")
 
   err = filesystem.MoveFileToPostedDirectory(conf.Filesystem.ImageDirectory, fileName)
   if err != nil {
-    log.Fatal(err)
+          log.Fatalf("Failed to move file to posted directory: %s", err)
   }
   log.Println("image file moved to posted directory")
 }
