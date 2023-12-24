a37507a2deb6e97c9f890f518706153a3ea050b0
Samuel Langley <samuellangley@intelligencefusion.co.uk>
2023-12-24 22:21:04 +0000

    add slog library for structured logging

--- src/main.go
+++ src/main.go
@@ -4,15 +4,18 @@ import (
 	"go-insta/config"
 	"go-insta/filesystem"
 	"go-insta/instagram"
-	"log"
+	"log/slog"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
-		log.Printf("failed to fetch env file: %s", err)
+		slog.Info("failed to fetch env file: %s", err)
 	}
 }
 
@@ -23,34 +26,34 @@ func main() {
 	// Get random image file from image directory
 	image, fileName, err := filesystem.GetRandomContent(conf.Filesystem.ImageDirectory)
 	if err != nil {
-		log.Fatalf("failed to fetch image content: %s", err)
+		slog.Info("failed to fetch image content: %s", err)
 	}
 
 	// Log into instagram account and create session
-	log.Println("logging into: " + conf.Instagram.Username)
+	slog.Info("logging into: " + conf.Instagram.Username)
 	sess, err := instagram.CreateSession(conf.Instagram.Username, conf.Instagram.Password)
 	if err != nil {
-		log.Fatalf("failed to log into account: %s", err)
+		slog.Info("failed to log into account: %s", err)
 	}
 
 	// Set proxy settings if provided
 	if len(conf.Network.ProxyAddress) > 0 {
 		err = sess.SetProxy(conf.Network.ProxyAddress, true, true)
 		if err != nil {
-			log.Fatalf("failed to set proxy: %s", err)
+			slog.Info("failed to set proxy: %s", err)
 		}
 	}
 
 	// Post random image file to Instagram
 	err = instagram.PostContent(sess, image)
 	if err != nil {
-		log.Fatalf("failed to post content: %s", err)
+		slog.Info("failed to post content: %s", err)
 	}
-	log.Println("uploaded content to instagram")
+	slog.Info("uploaded content to instagram")
 
 	err = filesystem.MoveFileToPostedDirectory(conf.Filesystem.ImageDirectory, fileName)
 	if err != nil {
-		log.Fatalf("failed to move file to posted directory: %s", err)
+		slog.Info("failed to move file to posted directory: %s", err)
 	}
-	log.Println("image file moved to posted directory")
+	slog.Info("image file moved to posted directory")
 }
