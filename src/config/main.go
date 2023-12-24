537ca5e4233ee9d59b0e46eb2a483b20e90ac8ad
Samuel Langley <samuellangley@intelligencefusion.co.uk>
2023-12-24 12:40:59 +0000

    move http_proxy configuration to config package

--- src/config/main.go
+++ src/config/main.go
@@ -9,41 +9,41 @@ type InstagramConfig struct {
 	Password string
 }
 
-type DropboxConfig struct {
-	Token string
-}
-
 type FilesystemConfig struct {
 	ImageDirectory string
 }
 
+type NetworkConfig struct {
+	ProxyAddress string
+}
+
 type Config struct {
-	Instagram InstagramConfig
+	Instagram  InstagramConfig
 	Filesystem FilesystemConfig
-	Dropbox DropboxConfig
+	Network    NetworkConfig
 }
 
 // New returns a new Config struct
 func New() *Config {
-    return &Config{
-    Instagram: InstagramConfig{
-	    Username: getEnv("INSTAGRAM_USERNAME", ""),
-	    Password: getEnv("INSTAGRAM_PASSWORD", ""),
-	},
-	Dropbox: DropboxConfig{
-		Token: getEnv("DROPBOX_ACCESS_TOKEN", ""),
-	},
-	Filesystem: FilesystemConfig{
-		ImageDirectory: "./images/" + getEnv("INSTAGRAM_USERNAME", ""),
-	},
-    }
+	return &Config{
+		Instagram: InstagramConfig{
+			Username: getEnv("INSTAGRAM_USERNAME", ""),
+			Password: getEnv("INSTAGRAM_PASSWORD", ""),
+		},
+		Filesystem: FilesystemConfig{
+			ImageDirectory: "./images/" + getEnv("INSTAGRAM_USERNAME", ""),
+		},
+		Network: NetworkConfig{
+			ProxyAddress: getEnv("HTTP_PROXY", ""),
+		},
+	}
 }
 
 // Simple helper function to read an environment or return a default value
 func getEnv(key string, defaultVal string) string {
-    if value, exists := os.LookupEnv(key); exists {
-	return value
-    }
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
 
-    return defaultVal
+	return defaultVal
 }
--- src/main.go
+++ src/main.go
@@ -1,54 +1,56 @@
 package main
 
 import (
-        "log"
-        "os"
-  "go-insta/instagram"
-  "go-insta/filesystem"
-  "go-insta/config"
-  "github.com/joho/godotenv"
+	"go-insta/config"
+	"go-insta/filesystem"
+	"go-insta/instagram"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func init() {
-  // loads values from .env into the system
-  if err := godotenv.Load(); err != nil {
-      log.Printf("No .env file found: %s", err)
-  }
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Printf("No .env file found: %s", err)
+	}
 }
 
 func main() {
-  // Generate new config object from config package
-  conf := config.New()
+	// Generate new config object from config package
+	conf := config.New()
 
-  // Get random image file from image directory
-  image, fileName, err := filesystem.GetRandomContent(conf.Filesystem.ImageDirectory)
-  if err != nil {
-          log.Fatalf("failed to fetch image content: %s", err)
-  }
+	// Get random image file from image directory
+	image, fileName, err := filesystem.GetRandomContent(conf.Filesystem.ImageDirectory)
+	if err != nil {
+		log.Fatalf("failed to fetch image content: %s", err)
+	}
 
-  // Log into instagram account and create session
-  log.Println("logging into: " + conf.Instagram.Username)
-  sess, err := instagram.CreateSession(conf.Instagram.Username, conf.Instagram.Password)
-  if err != nil {
-          log.Fatalf("failed to log into account: %s", err)
-  }
+	// Log into instagram account and create session
+	log.Println("logging into: " + conf.Instagram.Username)
+	sess, err := instagram.CreateSession(conf.Instagram.Username, conf.Instagram.Password)
+	if err != nil {
+		log.Fatalf("failed to log into account: %s", err)
+	}
 
-  if len(os.Getenv("HTTP_PROXY")) > 0 { 
-    err = sess.SetProxy(os.Getenv("HTTP_PROXY"), true, true)
-    if err != nil {
-            log.Fatalf("failed to set proxy: %s", err)
-            }
-  }
-  // Post random image file to Instagram
-  err = instagram.PostContent(sess, image)
-  if err != nil {
-          log.Fatalf("Failed to post content: %s", err)
-  }
-  log.Println("uploaded content to instagram")
+	// Set proxy settings if provided
+	if len(conf.Network.ProxyAddress) > 0 {
+		err = sess.SetProxy(conf.Network.ProxyAddress, true, true)
+		if err != nil {
+			log.Fatalf("Failed to set proxy: %s", err)
+		}
+	}
 
-  err = filesystem.MoveFileToPostedDirectory(conf.Filesystem.ImageDirectory, fileName)
-  if err != nil {
-          log.Fatalf("Failed to move file to posted directory: %s", err)
-  }
-  log.Println("image file moved to posted directory")
+	// Post random image file to Instagram
+	err = instagram.PostContent(sess, image)
+	if err != nil {
+		log.Fatalf("Failed to post content: %s", err)
+	}
+	log.Println("uploaded content to instagram")
+
+	err = filesystem.MoveFileToPostedDirectory(conf.Filesystem.ImageDirectory, fileName)
+	if err != nil {
+		log.Fatalf("Failed to move file to posted directory: %s", err)
+	}
+	log.Println("image file moved to posted directory")
 }
